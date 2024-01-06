dfe79779b08610ffbf48be4fdba14f68f49451e4
VikasMeshram2708 <meshramvikas744@gmail.com>
2024-01-06 21:50:48 +0530

    :sparkles: Added dark and light mode functinality.

--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,39 @@
 "use client";
 
 import Link from "next/link";
-import { useEffect, useState } from "react";
+import { useTheme } from "next-themes";
 
 export default function Home() {
-  const [toggleTheme, setToggleTheme] = useState("light");
+  const { setTheme } = useTheme();
 
-  const handleThemeToggler = () => {
-    setToggleTheme(toggleTheme === "light" ? "dark" : "light");
-  };
+  function lightThemeEnabler() {
+    setTheme("light");
+  }
 
-  useEffect(() => {
-    if (toggleTheme === "light") {
-      document.documentElement.classList.remove("dark");
-      document.documentElement.classList.add("light");
-    } else {
-      document.documentElement.classList.remove("light");
-      document.documentElement.classList.add("dark");
-    }
-  }, [toggleTheme]);
+  function darkThemeEnabler() {
+    setTheme("dark");
+  }
+
   return (
-    <main className="flex min-h-screen dark:bg-black dark:text-white flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen  flex-col items-center justify-between p-24">
       <h1>Home Page</h1>
       <p>
-        <Link href="/users">
-          Users
-        </Link>
+        <Link href="/users">Users</Link>
       </p>
       <button
-        onClick={handleThemeToggler}
+        onClick={lightThemeEnabler}
         type="button"
         className="bg-purple-600 border-4 text-white font-[600] border-purple-800 px-4 py-4 rounded-full"
       >
-        {toggleTheme === "light" ? "Light Mode Enabled" : "Dark Mode Enabled"}
+        Light Theme
+      </button>
+
+      <button
+        onClick={darkThemeEnabler}
+        type="button"
+        className="bg-black border-4 text-white font-[600] border-purple-800 px-4 py-4 rounded-full"
+      >
+        Light Theme
       </button>
     </main>
   );
